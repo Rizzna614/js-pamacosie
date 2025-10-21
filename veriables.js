1aab306ef00308d68e498eb1a5d9e90bf3042a34
User <nikolajevs04@gmail.com>
2025-10-21 10:53:19 +0300

    commit

---
+++ veriables.js
@@ -0,0 +1,14 @@
+let groupName = "PT2025";
+let size = 25;
+let isActive = false;
+const EULER = 2.718;
+
+
+//lai nodefinetu vertibu ir = bet lai parbauditu ir ==
+if (isActive == true) {
+    console.log("true")
+} else {
+    console.log("false")
+};
+
+
